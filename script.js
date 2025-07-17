26a2846816cac16c0699bc1085361852589fbc61
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-17 06:26:43 -0300

    Declare some variables

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const display = document.querySelector(".calculation");
 const buttons = document.querySelectorAll(".btn");
+
+let operatorsList = ["+", "-", "*", "/"];
+let digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+let decimaClicked = false;
+let num1 = "";
+let num2 = "";
+let operator;
+let inNum1 = true;
+let resultShown = false;
