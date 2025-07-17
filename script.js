466639ccbdb90277e5f9011e8c5313e7cb28d531
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-17 06:27:54 -0300

    Write the event handler

--- script.js
+++ script.js
@@ -93,3 +93,66 @@ function resetValues() {
   inNum1 = true;
   resultShown = false;
 }
+
+function handleCalc(e, inputValue) {
+  let buttonPressed = inputValue;
+
+  if (isNumber(buttonPressed) || (!decimaClicked && isDecimal(buttonPressed))) {
+    if (isDecimal(buttonPressed)) {
+      decimaClicked = true;
+    }
+    if (inNum1) {
+      if (resultShown) {
+        num1 = buttonPressed;
+        showInDisplay(num1);
+      } else {
+        num1 += buttonPressed;
+        showInDisplay(num1);
+      }
+    } else {
+      num2 += buttonPressed;
+      showInDisplay(num2);
+    }
+  } else if (isOperator(buttonPressed)) {
+    if (operator && num2) {
+      let result = operate(+num1, operator, +num2);
+      showInDisplay(result);
+      resultShown = true;
+      decimaClicked = false;
+      num1 = result;
+      num2 = "";
+    } else {
+      if (num1 && !operator) {
+        changeOperand();
+        decimaClicked = false;
+      }
+
+      clearDisplay();
+    }
+    operator = buttonPressed;
+  } else if (isEqualButton(buttonPressed) && num1 && num2 && operator) {
+    let result = operate(+num1, operator, +num2);
+    if (result === "Infinity" || result === -"Infinity") {
+      clearDisplay();
+      resetValues();
+      return alert("You cannot divide by 0!!!");
+    }
+    showInDisplay(result);
+    resetValues();
+    num1 = result;
+    resultShown = true;
+  } else if (isClearButton(buttonPressed)) {
+    clearDisplay();
+    resetValues();
+  } else if (isBackspace(e.currentTarget.id) || e.key === "Backspace") {
+    if (inNum1) {
+      num1 += "";
+      num1 = num1.slice(0, num1.length - 1);
+      showInDisplay(num1);
+    } else {
+      num2 += "";
+      num2 = num2.slice(0, num2.length - 1);
+      showInDisplay(num2);
+    }
+  }
+}
