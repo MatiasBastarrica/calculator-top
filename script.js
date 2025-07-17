201e529390bed49720c338f9a42c0a5fc76bf190
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-17 06:27:29 -0300

    Write the utility functions

--- script.js
+++ script.js
@@ -10,3 +10,86 @@ let num2 = "";
 let operator;
 let inNum1 = true;
 let resultShown = false;
+
+function add(operand1, operand2) {
+  return operand1 + operand2;
+}
+
+function subtract(operand1, operand2) {
+  return operand1 - operand2;
+}
+
+function multiply(operand1, operand2) {
+  return operand1 * operand2;
+}
+
+function divide(operand1, operand2) {
+  return operand1 / operand2;
+}
+
+function isOperator(buttonValue) {
+  return operatorsList.includes(buttonValue);
+}
+
+function isNumber(buttonValue) {
+  return digits.includes(buttonValue);
+}
+
+function isDecimal(buttonValue) {
+  return buttonValue === ".";
+}
+
+function isEqualButton(buttonValue) {
+  return buttonValue === "=";
+}
+
+function isClearButton(buttonValue) {
+  return buttonValue === "Clear";
+}
+
+function isBackspace(buttonValue) {
+  return buttonValue === "backspace" || buttonValue === "Backspace";
+}
+
+function showInDisplay(value) {
+  display.textContent = value;
+}
+
+function operate(operand1, operator, operand2) {
+  switch (operator) {
+    case "+":
+      result = add(operand1, operand2);
+      break;
+    case "-":
+      result = subtract(operand1, operand2);
+      break;
+    case "*":
+      result = multiply(operand1, operand2);
+      break;
+    case "/":
+      result = divide(operand1, operand2);
+      break;
+  }
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return (result = result.toFixed(2));
+  }
+}
+
+function clearDisplay() {
+  display.textContent = "";
+}
+
+function changeOperand() {
+  inNum1 = inNum1 ? false : true;
+}
+
+function resetValues() {
+  decimaClicked = false;
+  num1 = "";
+  num2 = "";
+  operator = "";
+  inNum1 = true;
+  resultShown = false;
+}
